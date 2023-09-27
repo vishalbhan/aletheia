7c463070f8d90b5edfdf20394bacdf2992d3dda1
Vishal Bhan <vishalbhan312@gmail.com>
2023-09-27 18:25:11 +0530

    updated form to use formspark

--- cms/schemas/index.js
+++ cms/schemas/index.js
@@ -3,11 +3,13 @@ import testimonials from "./testimonials";
 import tutors from "./tutors";
 import titleDesc from "./titleDesc";
 import services from "./services";
+import institutions from "./institutions";
 
 export const schemaTypes = [
   pageContent,
   testimonials,
   tutors,
   services,
+  institutions,
   titleDesc,
 ]
--- components/EnquiryForm.js
+++ components/EnquiryForm.js
@@ -1,96 +1,188 @@
-import { useState, useEffect, useRef } from 'react'
-import { BsArrowRight } from 'react-icons/bs';
+import { useFormspark } from "@formspark/use-formspark";
+import { useState, useEffect, useCallback } from "react";
+import { BsArrowRight } from "react-icons/bs";
+
+const FORMSPARK_FORM_ID = "7eIXKpP6";
 
 export default function EnquiryForm() {
+  const [formStatus, setFormStatus] = useState("");
+  const [submit, submitting] = useFormspark({ formId: FORMSPARK_FORM_ID });
+  const [formData, setFormData] = useState({
+    parent_name: "",
+    parent_email: "",
+    student_name: "",
+    curriculum: "",
+    service_required: "",
+    message: "",
+  });
 
-    const [formStatus, setFormStatus] = useState('')
-    const formRef = useRef()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await submit({ ...formData });
+    setFormStatus("sent")
+  };
 
-    useEffect(() => {
-        formRef.current.addEventListener("submit", handleSubmit);
-    }, [])
+  const handleChange = useCallback(
+    (type) => (event) => {
+      setFormData({ ...formData, [type]: event.target.value });
+    },
+    [formData]
+  );
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        let myForm = document.getElementById('enquiryForm');
-        let formData = new FormData(myForm)
-        fetch('/', {
-            method: 'POST',
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams(formData).toString()
-        })
-        .then(() => setFormStatus('sent'))
-        .catch((error) => alert(error))
-    }
+  return (
+    <form id="enquiryForm" onSubmit={handleSubmit}>
+      <div className="input-group">
+        <label htmlFor="parent-name">Name of Parent/Guardian</label>
+        <input
+          type="text"
+          id="parent-name"
+          name="parent-name"
+          onChange={handleChange("parent_name")}
+          required
+        />
+      </div>
 
-    return (
-        <form ref={formRef} id="enquiryForm" name="enquiry-form" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="enquiry-form" />
-            <div className="input-group">
-                <label htmlFor="parent-name">Name of Parent/Guardian</label>
-                <input type="text" id="parent-name" name="parent-name" required />
-            </div>
+      <div className="input-group">
+        <label htmlFor="parent-email">Parent/Guardian Email Address</label>
+        <input
+          type="email"
+          id="parent-email"
+          name="parent-email"
+          onChange={handleChange("parent_email")}
+          required
+        />
+      </div>
 
-            <div className="input-group">
-                <label htmlFor="parent-email">Parent/Guardian Email Address</label>
-                <input type="email" id="parent-email" name="parent-email" required />
-            </div>
+      <div className="input-group">
+        <label htmlFor="student-name">Name of Student</label>
+        <input
+          type="text"
+          id="student-name"
+          name="student-name"
+          onChange={handleChange("student_name")}
+          required
+        />
+      </div>
 
-            <div className="input-group">
-                <label htmlFor="student-name">Name of Student</label>
-                <input type="text" id="student-name" name="student-name" required />
-            </div>
+      <div className="input-group">
+        <label htmlFor="current-curriculum">Current Curriculum</label>
+        <input
+          type="text"
+          id="current-curriculum"
+          name="current-curriculum"
+          placeholder="e.g. IB MYP, GCSE, IB Diploma, A-Level, etc."
+          onChange={handleChange("curriculum")}
+          required
+        />
+      </div>
 
-            <div className="input-group">
-                <label htmlFor="current-curriculum">Current Curriculum</label>
-                <input type="text" id="current-curriculum" name="current-curriculum" placeholder="e.g. IB MYP, GCSE, IB Diploma, A-Level, etc." required />
-            </div>
+      <div className="input-group">
+        <label>Service Required</label>
+        <div className="radio-container">
+          <div className="radio-group">
+            <input
+              className="radio-input"
+              type="radio"
+              id="mid-year"
+              name="service-required"
+              onChange={handleChange("service_required")}
+              value="Mid-Year"
+              required
+            />
+            <label className="radio-label" tabIndex="0" htmlFor="mid-year">
+              Mid-Year
+            </label>
+          </div>
+          <div className="radio-group">
+            <input
+              className="radio-input"
+              type="radio"
+              id="6th-form"
+              name="service-required"
+              onChange={handleChange("service_required")}
+              value="6th Form"
+            />
+            <label className="radio-label" tabIndex="0" htmlFor="6th-form">
+              6th Form
+            </label>
+          </div>
+          <div className="radio-group">
+            <input
+              className="radio-input"
+              type="radio"
+              id="undergraduate"
+              name="service-required"
+              onChange={handleChange("service_required")}
+              value="Undergraduate"
+            />
+            <label className="radio-label" tabIndex="0" htmlFor="undergraduate">
+              Undergraduate
+            </label>
+          </div>
+          <div className="radio-group">
+            <input
+              className="radio-input"
+              type="radio"
+              id="graduate"
+              name="service-required"
+              onChange={handleChange("service_required")}
+              value="Graduate"
+            />
+            <label className="radio-label" tabIndex="0" htmlFor="graduate">
+              Graduate
+            </label>
+          </div>
+          <div className="radio-group">
+            <input
+              className="radio-input"
+              type="radio"
+              id="university-advice"
+              name="service-required"
+              onChange={handleChange("service_required")}
+              value="University Advice"
+            />
+            <label
+              className="radio-label"
+              tabIndex="0"
+              htmlFor="university-advice"
+            >
+              University Advice
+            </label>
+          </div>
+          <div className="radio-group">
+            <input
+              className="radio-input"
+              type="radio"
+              id="careers-advice"
+              name="service-required"
+              onChange={handleChange("service_required")}
+              value="Careers Advice"
+            />
+            <label
+              className="radio-label"
+              tabIndex="0"
+              htmlFor="careers-advice"
+            >
+              Careers Advice
+            </label>
+          </div>
+        </div>
+      </div>
 
-            <div className="input-group">
-                <label>Service Required</label>
-                <div className="radio-container">
-                    <div className="radio-group">
-                        <input className="radio-input" type="radio" id="mid-year" name="service-required" value="Mid-Year" required />
-                        <label className="radio-label" tabIndex="0" htmlFor="mid-year">Mid-Year</label>
-                    </div>
-                    <div className="radio-group">
-                        <input className="radio-input" type="radio" id="6th-form" name="service-required" value="6th Form" />
-                        <label className="radio-label" tabIndex="0" htmlFor="6th-form">6th Form</label>
-                    </div>
-                    <div className="radio-group">
-                        <input className="radio-input" type="radio" id="undergraduate" name="service-required" value="Undergraduate" />
-                        <label className="radio-label" tabIndex="0" htmlFor="undergraduate">Undergraduate</label>
-                    </div>
-                    <div className="radio-group">
-                        <input className="radio-input" type="radio" id="graduate" name="service-required" value="Graduate" />
-                        <label className="radio-label" tabIndex="0" htmlFor="graduate">Graduate</label>
-                    </div>
-                    <div className="radio-group">
-                        <input className="radio-input" type="radio" id="university-advice" name="service-required" value="University Advice" />
-                        <label className="radio-label" tabIndex="0" htmlFor="university-advice">University Advice</label>
-                    </div>
-                    <div className="radio-group">
-                        <input className="radio-input" type="radio" id="careers-advice" name="service-required" value="Careers Advice" />
-                        <label className="radio-label" tabIndex="0" htmlFor="careers-advice">Careers Advice</label>
-                    </div>
-                </div>
-            </div>
+      <div className="input-group">
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" rows="4" onChange={handleChange("message")} />
+      </div>
 
-            <div className="input-group">
-                <label htmlFor="message">Message</label>
-                <textarea id="message" name="message" rows="4" />
-            </div>
-
-            <div className="text-right span-2">
-                {
-                    formStatus === 'sent' ? (
-                        <p>Enquiry received, we'll be in touch soon!</p>
-                    ) : (
-                        <button>Send Enquiry <BsArrowRight /></button>
-                    )
-                }
-            </div>
-
-        </form>
-    )
+      <div className="text-right span-2">
+        {formStatus === "sent" ? (
+          <p>Enquiry received, we'll be in touch soon!</p>
+        ) : (
+          <button disabled={submitting}>
+            Send Enquiry <BsArrowRight />
+          </button>
+        )}
+      </div>
+    </form>
+  );
 }
