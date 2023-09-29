a24660348003f6bce0cf20eb9457f14c4b6a4d0c
Vishal Bhan <vishalbhan312@gmail.com>
2023-09-30 01:05:16 +0530

    added modal bio

--- components/TutorsGrid.js
+++ components/TutorsGrid.js
@@ -3,13 +3,19 @@ import Image from "next/image";
 import { urlFor } from "utils/urlFor";
 
 export default function TutorsGrid({ tutorsContent }) {
+  const [selectedTutor, setSelectedTutor] = useState(null)
   const modalRef = useRef()
+
+  const showModal = (tutor) => {
+    setSelectedTutor(tutor)
+    modalRef.current.showModal()
+  }
 
   return (
     <>
     <div className="tutors-grid fade-up" data-scroll>
       {tutorsContent.map((tutor, index) => (
-        <div className="tutors-grid-item" key={`tutor-${index}`}>
+        <div className="tutors-grid-item" key={`tutor-${index}`} onClick={() => showModal(tutor)}>
           <div className="tutors-grid-inner">
             <div className="tutor-image">
               <Image
@@ -31,9 +37,39 @@ export default function TutorsGrid({ tutorsContent }) {
       ))}
     </div>
 
-    <dialog ref={modalRef}>
-
+    <dialog className="tutors-modal" ref={modalRef}>
+      <div className="modal-content">
+        <div className="modal-header">
+          <a className="modal-close" onClick={() => modalRef.current.close()}>
+            &times;
+          </a>
+        </div>
+        <div>
+          {selectedTutor && (
+            <>
+              <div className="modal-tutor-image tutor-image">
+                <Image
+                  src={urlFor(selectedTutor.image).url()}
+                  width={272}
+                  height={350}
+                  objectFit="cover"
+                  priority
+                />
+              </div>
+              <div className="tutor-details">
+                <div className="tutor-name">{selectedTutor.name}</div>
+                <div className="tutor-qualification">
+                  <em>{selectedTutor.qualification}</em>
+                </div>
+                <p>{selectedTutor.about}</p>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </dialog>
     </>
   );
 }
+
+
